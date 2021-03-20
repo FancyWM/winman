ad20b66b2a309263aa2eece48515c23a1bf3f735
Veselin Karaganev <vesko.karaganev@gmail.com>
2021-03-20 07:37:57 +0200

    Uncouple Win32 backend

---
+++ WinMan/WorkspaceExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WinMan
+{
+    public static class WorkspaceExtensions
+    {
+        public static IReadOnlyList<IWindow> GetCurrentDesktopSnapshot(this IWorkspace workspace)
+        {
+            return workspace.GetSnapshot()
+                .Where(workspace.VirtualDesktopManager.CurrentDesktop.HasWindow)
+                .ToList();
+        }
+    }
+}
